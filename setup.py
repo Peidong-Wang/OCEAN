fa183166833390bc9d183595e16e77e9f7241ee0
Peidong Wang <peidong333333@gmail.com>
2020-10-21 11:45:20 -0500

    ocean init

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import os, sys
+import shutil
+import datetime
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+readme = open('README.md').read()
+
+VERSION = '0.0.4'
+
+requirements = [
+    'torch',
+]
+
+# import subprocess
+# commit_hash = subprocess.check_output("git rev-parse HEAD", shell=True).decode('UTF-8').rstrip()
+# VERSION += "_" + str(int(commit_hash, 16))[:8]
+VERSION += "_" + datetime.datetime.now().strftime('%Y%m%d%H%M')[2:]
+# print(VERSION)
+
+setup(
+    # Metadata
+    name='ocean',
+    version=VERSION,
+    author='Peidong Wang',
+    author_email='peidong-wang@outlook.com',
+    url='',
+    description='A tool to count the operations of end-to-end ASR models.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='MIT',
+
+    # Package info
+    packages=find_packages(exclude=('*test*',)),
+
+    #
+    zip_safe=True,
+    install_requires=requirements,
+
+    # Classifiers
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
